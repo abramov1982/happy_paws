83845db206fc591364a6c93ae1706a963fd6a419
Abramov Ilya <abramovio1982@gmail.com>
2020-08-09 19:47:37 +0300

    deploy ready

--- happy_paws/settings.py
+++ happy_paws/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '192.168.1.68']
+ALLOWED_HOSTS = ['127.0.0.1', 'infinite-temple-99749.herokuapp.com']
 
 
 # Application definition
